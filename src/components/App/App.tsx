16702e8d0bff03467df6586e0dcbbda2d2c42a94
mankov <mankov@farzoom.com>
2021-08-11 22:37:57 +0700

    first commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import './App.scss';
+
+class App extends Component {
+    render() {
+        return (
+            <div className='App'>
+                <h1>Test App</h1>
+            </div>
+        );
+    }
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        open: true,
+        hot: true,
+        port: process.env.PORT || 8080,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react",
+                            "@babel/preset-typescript",
+                        ],
+                    },
+                },
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./www/index.html"
+        })
+    ]
+};
